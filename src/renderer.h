a6f832df2c8ed2187b1ca49ce7b0f2ffeb80e286
Mohamad Ashraf Salama <mohamadashrafaast@gmail.com>
2022-01-17 20:03:02 +0000

    add Renderer class header

---
+++ src/renderer.h
@@ -0,0 +1,37 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "game.h"
+
+class Renderer {
+public:
+    Renderer(int windowWidth, int windowHeight, Game* game);
+
+    void init();
+    void display();
+    void drawGrid();
+    void drawCell(int x, int y, bool alive);
+
+    void screenToGrid(int screenX, int screenY, int* gridX, int* gridY);
+
+    int getWindowWidth() const;
+    int getWindowHeight() const;
+
+private:
+    int windowWidth;
+    int windowHeight;
+    Game* game;
+
+    float cellWidth;
+    float cellHeight;
+
+    void updateCellSize();
+};
+
+#endif
